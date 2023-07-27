6f35e175b24e1ecc4a6afb1247af1157f5d0408d
Kat Hoffman <hoffmakl@umich.edu>
2023-07-27 16:28:42 -0400

    add oud components for diagnosis codes, nonfatal overdose, and probable misuse

---
+++ scripts/06_define_OUD_components/define_oud_poison.R
@@ -0,0 +1,140 @@
+################################################################################
+################################################################################
+###  Define OUD poison using Cochran's codes
+################################################################################
+################################################################################
+
+# Set up -----------------------------------------------------------------------
+
+# load libraries
+library(arrow)
+library(tidyverse)
+library(tidylog)
+library(lubridate)
+library(data.table)
+library(tictoc)
+library(future)
+library(furrr)
+library(doParallel)
+# options(cores=50)
+registerDoParallel()
+plan(multicore)
+getDoParWorkers()
+
+source("scripts/ICD_codes/OUD.R")
+
+# Load necessary files ---------------------------------------------------------
+
+dts_cohorts <- open_dataset("data/tafdedts/dts_cohorts.parquet") |>
+    collect() |> 
+    mutate(index = rep(1:32, length.out=n()))
+
+# Read in data files ---------------------------------------------------------
+
+td <- "/home/data/12201/" # directory of interest
+
+# read in IPH (inpatient hospital)
+files <- paste0(list.files(td, pattern = "TAFIPH", recursive = TRUE))
+iph <- open_dataset(paste0(td, files), format="parquet")
+
+# read in OTH (other services baseline)
+files <- paste0(list.files(td, pattern = "TAFOTH", recursive = TRUE))
+oth <- open_dataset(paste0(td, files), format="parquet")
+
+# Filter arrow files by cohort ---------------------------------------------------------
+
+all <- 1:32
+td <- "data/oud_info/tmp_splits_oud_poison_dt/" 
+files <- paste0(list.files(td, pattern = "*.parquet", recursive = TRUE)) 
+done <- unique(parse_number(files))
+still <- all[-which(all %in% done)]
+
+still <- 1:32
+foreach(i = still) %dopar%
+    {print(paste(i, Sys.time()))
+        
+        iter <- dts_cohorts |> filter(index == i) |> pull(BENE_ID)
+        
+        oth_cohort <- # first extract the other services data
+            oth |>
+            filter(!is.na(DGNS_CD_1)) |>
+            filter(BENE_ID %in% iter) |> 
+            mutate(SRVC_BGN_DT = case_when(is.na(SRVC_BGN_DT) ~ SRVC_END_DT, TRUE ~ SRVC_BGN_DT)) |>
+            filter(SRVC_BGN_DT >= as.Date("2016-01-01")) |> # save space, keep only DG codes after 2016
+            select(BENE_ID, SRVC_BGN_DT,  SRVC_END_DT, contains("DGNS_CD")) |>
+            collect()
+        
+        iph_cohort <- # then extract IPH data
+            iph |>
+            filter(BENE_ID %in% iter) |> 
+            mutate(SRVC_BGN_DT = case_when(is.na(SRVC_BGN_DT) ~ SRVC_END_DT, TRUE ~ SRVC_BGN_DT)) |>
+            filter(SRVC_BGN_DT >= as.Date("2016-01-01")) |> # save space, keep only DG codes after 2016
+            select(BENE_ID, ADMSN_DT, SRVC_BGN_DT, contains("DGNS_CD")) |>
+            collect()
+        
+        print(paste(i, "subset", Sys.time()))
+        
+        # Create OUD component date variables from IPH and OTH ---------------------------------------------------------
+        
+        oth_all_vars <-
+            oth_cohort |> # define oud variable of interest via codes
+            mutate(oud_poison_dt = case_when(DGNS_CD_1 %in% cochran_codes_poison ~ SRVC_BGN_DT,
+                                            DGNS_CD_2 %in% cochran_codes_poison ~ SRVC_BGN_DT)) |>
+            drop_na(oud_poison_dt) |> # drop anyone who doesn't have a date for the oud var of interest
+            distinct(BENE_ID, oud_poison_dt)
+        
+        iph_all_vars <-
+            iph_cohort |> # for IPH, first need to pivot to long format because there are 12 DG codes
+            pivot_longer(cols = contains("DGNS_CD"), names_to = "dg_num", values_to = "cd") |> # switch to long format data because there are 12 dx codes and IPH isn't too big
+            drop_na(cd) |> # drop any missing icd code (cd) rows
+            mutate(oud_poison_dt = case_when(cd %in% cochran_codes_poison ~ SRVC_BGN_DT)) |>  # define oud variable of interest via codes
+            drop_na(oud_poison_dt) |> # drop anyone who doesn't have a date for the oud var of interest
+            distinct(BENE_ID, oud_poison_dt)
+        
+        all <- bind_rows(oth_all_vars, iph_all_vars) # stack the columns (BENE_ID and oud var of interest) on top of each other
+        
+        write_parquet(all, paste0("data/oud_info/tmp_splits_oud_poison_dt/", i, ".parquet")) # save the split
+        
+    }
+
+# Collect all the splits and merge to one data set
+
+td <- "data/oud_info/tmp_splits_oud_poison_dt/" 
+files <- paste0(list.files(td, pattern = "*.parquet", recursive = TRUE)) 
+all_oud_poison_dt <-
+    open_dataset(paste0(td, files), format="parquet") |>
+    select(BENE_ID, oud_poison_dt) |>
+    collect()
+
+# Keep only the minimum date of the OUD variable of interest that occurs after the washout period starts for that beneficiary ID 
+min_oud_poison_dt <- 
+    all_oud_poison_dt |>
+    left_join(dts_cohorts |> select(BENE_ID, washout_start_dt)) |>
+    filter(oud_poison_dt >= washout_start_dt) |> # only keep oud poison diagnosis codes that occur during washout period
+    select(BENE_ID, oud_poison_dt) |>
+    arrange(oud_poison_dt) |>
+    distinct(BENE_ID, .keep_all = T) # only keep first date after washout (already arranged by date)
+
+
+oud_poison_clean <- 
+    dts_cohorts |>
+    left_join(min_oud_poison_dt) |>
+    mutate(oud_poison_washout_cal = case_when(oud_poison_dt %within% interval(washout_start_dt, washout_cal_end_dt) ~ 1,
+                                                TRUE ~ 0),
+           oud_poison_washout_12mos_cal = case_when(oud_poison_dt %within% interval(washout_start_dt, washout_12mos_end_dt) ~ 1,
+                                              TRUE ~ 0),
+           oud_poison_washout_cont = case_when(oud_poison_dt %within% interval(washout_start_dt, washout_cont_end_dt) ~ 1,
+                                                 TRUE ~ 0),
+           oud_poison_study_cal = case_when(oud_poison_dt %within% interval(washout_start_dt, study_cal_end_dt) ~ 1,
+                                              is.na(study_cal_end_dt) & oud_poison_dt > washout_start_dt ~ 1,
+                                              TRUE ~ 0),
+           oud_poison_study_cont = case_when(oud_poison_dt %within% interval(washout_start_dt, study_cont_end_dt) ~ 1,
+                                               is.na(study_cont_end_dt) & oud_poison_dt > washout_start_dt ~ 1,
+                                               TRUE ~ 0)) |>
+    select(BENE_ID, oud_poison_dt, 
+           oud_poison_washout_cal, oud_poison_washout_12mos_cal,
+           oud_poison_washout_cont,
+           oud_poison_study_cal, oud_poison_study_cont)
+
+write_rds(oud_poison_clean, "data/final/oud_poison.rds")
+
---
+++ scripts/06_define_OUD_components/define_oud_probable_misuse.R
@@ -0,0 +1,244 @@
+################################################################################
+################################################################################
+###  ADD OUD PROBABLE MISUSE VARIABLE
+###  Kat Hoffman, Feb 2023
+###  Purpose: define OUD via various definitions
+###  Output: 
+################################################################################
+################################################################################
+
+# Set up -----------------------------------------------------------------------
+
+# load libraries
+library(arrow)
+library(tidyverse)
+library(tidylog)
+library(lubridate)
+library(data.table)
+library(tictoc)
+library(future)
+library(furrr)
+library(doParallel)
+# options(cores=50)
+registerDoParallel()
+plan(multicore)
+getDoParWorkers()
+
+# Load necessary files ---------------------------------------------------------
+
+dts_cohorts <- open_dataset("data/tafdedts/dts_cohorts.parquet") |>
+    collect() |> 
+    mutate(index = rep(1:32, length.out=n()))
+
+opioids <- read_csv("input/NDC_codes/opioid_pain_rxs_clean.csv") |>
+    rename(NDC = ndc)
+
+#ndc_list_clean <- read_rds( "input/NDC_codes/ndc_list_clean.rds")
+#bup_ndc_dose_clean <- read_rds("input/NDC_codes/bup_ndc_dose_clean.rds")
+
+td <- "/home/data/12201/" # directory of interest
+
+# Read in RXL (pharmacy line)
+files <- paste0(list.files(td, pattern = "TAFRXL", recursive = TRUE))
+rxl <- open_dataset(paste0(td, files), format="parquet")
+
+files <- paste0(list.files(td, pattern = "TAFRXH", recursive = TRUE))
+rxh <- open_dataset(paste0(td, files), format="parquet")
+
+# extract prescriber / dispenser info from cohort of interest
+tic()
+prescribers <- 
+    rxh |>
+    select(BENE_ID, CLM_ID,
+           PRSCRBNG_PRVDR_ID, PRSCRBNG_PRVDR_NPI,
+           DSPNSNG_PRVDR_ID, DSPNSNG_PRVDR_NPI,
+           RX_FILL_DT) |>
+   collect()
+toc()
+
+tic()
+# extract drug info from cohort of interest and categorize script types
+ndcs <- rxl |>
+    select(BENE_ID, CLM_ID, contains("NDC"), DAYS_SUPPLY)|>
+    filter(NDC %in% opioids$NDC) |>
+    filter(!is.na(BENE_ID)) |>
+    collect() |>
+    left_join(opioids)
+toc() # 133 seconds
+
+prescribers_filter <- 
+    prescribers |>
+    filter(CLM_ID %in% ndcs$CLM_ID)
+
+saveRDS(prescribers_filter, "data/tmp/prescribers_filter.rds")
+
+all_opioids <- inner_join(ndcs, prescribers_filter) # only want to keep prescribers  
+
+saveRDS(all_opioids, "data/oud_info/all_opioids_prescribers_ndcs.rds")
+
+
+all_opioids <- read_rds("data/oud_info/all_opioids_prescribers_ndcs.rds")
+
+opioids_rxl_no_bup  <-
+    all_opioids |>
+    filter(!(drug_name == "Buprenorphine"))
+
+# for bup, only keep strength/day < 10
+opioid_pain_rxl_bup <-
+    all_opioids |>
+    filter(drug_name == "Buprenorphine", dosage_form == "TABLET") |>
+    mutate(pills_per_day = NDC_QTY/DAYS_SUPPLY,
+           strength_per_day = bup_strength_clean * pills_per_day) |>
+    filter(strength_per_day <= 10) 
+
+all_opioids_clean <-
+    opioids_rxl_no_bup |>
+    select(BENE_ID, RX_FILL_DT, PRSCRBNG_PRVDR_NPI, DSPNSNG_PRVDR_NPI, DAYS_SUPPLY) |>
+    full_join( opioid_pain_rxl_bup |> select(BENE_ID, RX_FILL_DT, PRSCRBNG_PRVDR_NPI, DSPNSNG_PRVDR_NPI, DAYS_SUPPLY)) |>
+    left_join(dts_cohorts |> select(BENE_ID, washout_start_dt))
+
+#Probable Opioid Misuse:  
+# number of opioid prescribers (<= 2 prescribers=0, 3-4 prescribers=1, >= 5 prescribers=2), 
+# number of pharmacies used for medication filling (<= 2 pharmacies=0, 3-4 pharmacies=1, >= 5 pharmacies=2),
+# days supplied of short-acting opioids, 
+# and days of supply of long-acting opioids (<=185 days=0, 186-240 days=1, >240 days=2)
+# over 6-month periods, summed and >= 5 categorized as probable misuse
+
+rolling_windows <- function(df, month_start){
+    print(paste0(month_start, Sys.time()) )
+    df |>
+        # first, define the 6 month time window of interest
+        mutate(start_month = washout_start_dt + months(month_start),
+               end_month = washout_start_dt + months(month_start) + 182) |>
+        # then, filter the prescriptions to only contain those filled within the relevant time frame
+        filter(RX_FILL_DT %within% interval(start_month, end_month)) |>
+        group_by(BENE_ID) |>
+        # count the number of unique 1) prescribing provider NPIs 2) dispensing providing NPIs and
+        # 3) sum the total days supply of all opioids given in the window
+        summarize(distinct_providers = n_distinct(PRSCRBNG_PRVDR_NPI),
+                  distinct_dispensers = n_distinct(DSPNSNG_PRVDR_NPI),
+                  total_days_supply = sum(DAYS_SUPPLY)) |>
+        ungroup() |>
+        # classify the scores using the probable misuse variable logic 
+        mutate(score_providers = case_when(distinct_providers <= 2 ~ 0,
+                                           distinct_providers <= 4 ~ 1,
+                                           distinct_providers >= 5 ~ 2),
+               score_dispensers = case_when(distinct_dispensers <= 2 ~ 0,
+                                            distinct_dispensers <= 4 ~ 1,
+                                            distinct_dispensers >= 5 ~ 2),
+               score_days_supply = case_when(total_days_supply <= 185 ~ 0,
+                                             total_days_supply <= 240 ~ 1,
+                                             total_days_supply > 240 ~ 2,
+                                             is.na(total_days_supply) ~ 0 # a few days supply are missing
+               ),
+               # sum the scores - will be an integer between 0-6
+               score_misuse = score_providers +  score_dispensers + score_days_supply) |>
+        # create (start) month variable as the ID
+        mutate(month = month_start) 
+}
+
+# # takes 9 min for month 0 to 6
+# start_time <- Sys.time()
+# tmp <- rolling_windows(opioids_check, 0)
+# end_time <- Sys.time()
+# time_elapsed <- difftime(end_time, start_time)
+# time_elapsed # 9 min
+
+# realized i also need to do month 0... and continue through max of 4 years on study (48-6 months = 42) because we don't know when cont time ends
+still <- c(0:42) # may need to go back and add the last 6 months
+foreach (time = still) %dopar% {
+    tmp <- rolling_windows(all_opioids_clean, time)
+    write_parquet(tmp, paste0("data/oud_info/tmp_splits_oud_misuse/", time, ".parquet"))
+}
+
+# reduce list output to one long data frame (only contains beneficiaries receiving any opioids)
+all_misuse_splits <- open_dataset("data/oud_info/tmp_splits_oud_misuse/")
+
+all_misuse_splits_c <-
+    all_misuse_splits |>
+    collect()
+
+
+# compare to months(6)
+all_misuse_splits_months <- open_dataset("data/oud_info/tmp_splits_oud_misuse/")
+
+all_misuse_splits_c_month <-
+    all_misuse_splits_months |>
+    collect()
+
+nrow(all_misuse_splits_c)
+nrow(all_misuse_splits_c_month)
+
+all_misuse_splits_c_month |> filter(month == 0) |> nrow()
+all_misuse_splits_c |> filter(month == 0) |> nrow()
+
+all_misuse_splits_c |>
+    filter(month == 0) |>
+    filter(score_misuse >= 5)
+
+all_misuse_splits_c_month |>
+    filter(month == 0) |>
+    filter(score_misuse >= 5)
+
+# make a key that is number of months on study (only need to do this for continuous cohort)
+study_months_key <-
+    dts_cohorts |>
+        mutate(max_study_months = interval(washout_start_dt, study_cont_end_dt) %/% months(1)) |>
+    select(BENE_ID, max_study_months) |>
+    # max months is 48 if they don't have a final study end date for the continuous enrollment interval
+    replace_na(list(max_study_months = 42))  # don't need to fill in for no washout continuous completion because they'll be excluded
+
+study_months_key
+
+all_misuse_splits_c_adj <-
+    all_misuse_splits_c |>
+    left_join(study_months_key) |>
+    filter(month <= max_study_months) # remove months that are greater than the max study (cont) months since washout
+
+# determine eligibility criteria here
+exclusion_misuse <-
+    all_misuse_splits_c_adj  |>
+    filter(month == 0) |>
+    select(BENE_ID:score_misuse) |>
+    mutate(cohort_exclusion_oud_misuse = case_when(score_misuse >= 5 ~ 1)) |>
+    select(BENE_ID, cohort_exclusion_oud_misuse) |>
+    right_join(dts_cohorts |> select(BENE_ID)) |>
+    replace_na(list(cohort_exclusion_oud_misuse = 0))
+
+study_cal_misuse <-
+    all_misuse_splits_c_adj  |>
+    filter(month < 18) |> # first 18 month rolling periods are indexed by 0:17... may need to fix this if we want to include info from after calendar years (months 25-31)
+    select(BENE_ID:score_misuse) |>
+    group_by(BENE_ID) |> # group by id to calculate the max score variables, and whether they had an oud misuse qualifying score in the calendar washout period
+    mutate(max_score_misuse = max(score_misuse),
+           oud_misuse_cal_any = case_when(max_score_misuse >= 5 ~ 1, TRUE ~ 0)) |>
+    group_by(BENE_ID, oud_misuse_cal_any) |> # group by both variables so that we keep them during "summarize" step
+    summarize(across(distinct_providers:total_days_supply, mean)) |>
+    rename(oud_misuse_cal_avg_distinct_providers = distinct_providers,
+           oud_misuse_cal_avg_distinct_dispensers = distinct_dispensers,
+           oud_misuse_cal_avg_total_days_supply = total_days_supply)
+
+study_cont_misuse <-
+    all_misuse_splits_c_adj  |>
+    select(BENE_ID:score_misuse) |>
+    group_by(BENE_ID) |>
+    group_by(BENE_ID) |> # group by id to calculate the max score variables, and whether they had an oud misuse qualifying score in the calendar washout period
+    mutate(max_score_misuse = max(score_misuse),
+           oud_misuse_cont_any = case_when(max_score_misuse >= 5 ~ 1, TRUE ~ 0)) |>
+    group_by(BENE_ID, oud_misuse_cont_any) |> # group by both variables so that we keep them during "summarize" step
+    summarize(across(distinct_providers:total_days_supply, mean))|>
+    rename(oud_misuse_cont_avg_distinct_providers = distinct_providers,
+           oud_misuse_cont_avg_distinct_dispensers = distinct_dispensers,
+           oud_misuse_cont_avg_total_days_supply = total_days_supply)
+
+oud_misuse_clean <- 
+    exclusion_misuse |>
+    left_join(study_cal_misuse) |>
+    left_join(study_cont_misuse) |>
+    replace_na(list( oud_misuse_cont_any = 0,
+                     oud_misuse_cal_any = 0)) # Don't replace oud_misuse_* variables with NA because we don't want them to be displayed in the table
+
+# save entire OUD misuse data frame in final folder
+saveRDS(oud_misuse_clean, "data/final/oud_misuse.rds")
+
+
